f4e47178994e00ef6ee5782f926c9ac7ad4d230b
Michael Rosa-Vourazeris <mrosavourazeris@gmail.com>
2021-03-28 16:55:13 -0500

    Created a employee, table and wrapper component.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import { useState } from 'react'
+import React, { Component } from "react";
 import './App.css';
+import Wrapper from './Components/Wrapper'
+import Table from './Components/Table'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
+        <Wrapper>
+          <Table />
+        </Wrapper>
       </header>
     </div>
   );
